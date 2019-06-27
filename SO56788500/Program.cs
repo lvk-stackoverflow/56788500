f70753eb44aae95a9af82f103399e67c10057b58
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2019-06-27 14:02:01 +0200

    Added example from question

--- SO56788500/Program.cs
+++ SO56788500/Program.cs
@@ -6,7 +6,7 @@ namespace SO56788500
     {
         static void Main(string[] args)
         {
-            var text = new TextSectionParser().Parse("123 {a {x|y}|b {k|l}|c {f|j}} 456");
+            var text = new TextSectionParser().Parse("My name is {John|Tom|Bob} and I live in {a {big|small} city|the {beautiful|large} country}");
 
             var rnd = new Random();
             for (int index = 0; index < 10; index++)
